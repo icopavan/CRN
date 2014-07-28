a18c3845135ef37ab09ae97941e01784a8dd01b5
Piyush Bhardwaj <bhardwaaj.piyush@gmail.com>
2014-07-29 02:28:23 +0530

    add plotter

---
+++ maxthrough_omni_ga_multiP5/plotter.m
@@ -0,0 +1,9 @@
+function [] = plotter( population )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+x  = population(:,1);
+y = population (:,2);
+plot(x,y,'*');
+%input('press enter to cont.') 
+end
+
