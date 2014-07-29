0fd75e31f53c5ff1e538911637aa8ef97db7cbc1
Piyush Bhardwaj <bhardwaaj.piyush@gmail.com>
2014-07-29 08:34:50 +0530

    removed journal_result.m

--- maxthrough_directional_ga_multiP5/journal_result.m
+++
@@ -1,8 +0,0 @@
- normalized_throughput = [];
-for (delta = 1:1) %1:5
-for (loc_no= 1:1) %1:100
-    delta
-    loc_no
-  normalized_throughput(delta,loc_no)  =   maxthrough_directional_deter(delta,loc_no);
-end
-end
